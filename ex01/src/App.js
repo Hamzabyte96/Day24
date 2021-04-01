4c5da42919821da9ffac9eabe0dcfc93d7a91e15
Hamzabyte96 <brcaninovichamza@yahoo.com>
2021-04-01 06:32:16 +0200

    izmjene

---
+++ ex01/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+  return (
+    <div className="App">
+      <button type="button" onClick={this.setMyStorage}></button>
+    </div>
+  );
+  }
+  setMyStorage = () => {
+    document.cookie = "Year=2021";
+    localStorage.setItem("Paragon", "yes, but Arena first");
+    sessionStorage.setItem("frontend", "React");
+   }
+   
+}
+
+export default App;
